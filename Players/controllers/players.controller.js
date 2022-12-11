6e0d3dca62ded651d58f3eb664e53c61273e0b45
limburamu <limburamu23@gmail.com>
2022-12-11 09:51:30 +0545

    Error debuged

--- Players/controllers/players.controller.js
+++ Players/controllers/players.controller.js
@@ -56,7 +56,7 @@ playersController.getPlayer = (req, res) => {
     if(playerResponse) {
         const playerId = parseInt(req.params.playerId);
         const clubIds = playerResponse.clubIds;
-        const clubResponse;
+        let clubResponse;
         const pos = clubIds.indexOf(clubId); 
 
         if(pos !== -1) {
@@ -74,4 +74,87 @@ playersController.getPlayer = (req, res) => {
         res.status(404).json('Player with the given player id is not found');
     }
 }
+
+
+playersController.deletePlayer = function(req, res) {
+    const id = parseInt(req.params.playerId);
+
+    let flag = 0;
+    
+    for(i = 0; i < players.length; i++)
+    {
+        if(id === players[i].id)
+        {
+            flag = 1;
+            console.log(players[i].id);
+            players.splice(i, 1);
+        }
+    }
+    if(flag == 0)
+    {
+        res.send('sorry');
+    } else {
+        res.send('player deleted');
+    }
+}
+
+playersController.updatePlayer = function(req, res) {
+    const id = parseInt(req.params.playerId)
+
+    let flag = 0;
+
+    console.log(req.body);
+
+    for(i = 0; i < players.length; i++)
+    {
+        if(id === players[i].id)
+        {
+            flag = 1;
+            players[i].name = req.body.name;
+            players[i].age = req.body.age;
+            players[i].address = req.body.address;
+            players[i].club = req.body.club;
+            console.log(players[i].id);
+        }
+    }
+    if(flag == 0)
+    {
+        res.send('sorry');
+    } else {
+        res.send('player updated');
+    }
+}
+
+playersController.addPlayer = function(req, res) {
+    console.log(req.body);
+    players.push(req.body);
+    res.status(201).send('Player added');
+}
+
+
+playersController.getClubs = function(req, res) {
+    const playerId = parseInt(req.params.playerrId)
+
+    let responseClubs = [];
+    let player;
+
+    players.forEach(t => {
+        if(t.id === playerId)
+        {
+            player = t;
+        }
+    });
+    for(i = 0; i < player.clubsIds.length; i++)
+    {
+        clubs.forEach(club => {
+            if(club.id === player.clubIds[i])
+            {
+                responseClub.push(subject);
+            }
+        })
+    }
+    res.status(200).json(responseClub);
+}
+
+
 module.exports = playersController
